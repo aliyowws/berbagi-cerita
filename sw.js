7765f869dd685190b8cdd3c7612cc8ed78dc908c
aliyowws <aliyahazzahri@gmail.com>
2025-06-12 20:20:50 +0700

    ubah logo notif

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll([...new Set(urlsToCache)])) // hindari duplikat
+    caches.open(CACHE_NAME).then(cache => cache.addAll([...new Set(urlsToCache)])) 
   );
   self.skipWaiting(); 
 });
